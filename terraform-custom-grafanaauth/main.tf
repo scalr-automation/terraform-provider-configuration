38337ba3aa7dbf418292731a33902b0d435d5750
Kostya Kotov <38981440+k-kotov@users.noreply.github.com>
2023-06-16 12:52:33 +0300

    Update main.tf

--- terraform-custom-grafanaauth/main.tf
+++ terraform-custom-grafanaauth/main.tf
@@ -1,26 +1,34 @@
+
 terraform {
-  required_providers {
-    grafanaauth = {
-      source   = "orendain/grafanaauth"
-//    source   = "github.com/orendain/grafanaauth" // for use during development
-      #versions = "0.0.3"
-    }
+   required_providers {
+     scalr = {
+       source  = "registry.scalr.io/scalr/scalr"
+       version = "1.0.0-rc38"
+     }
+   }
+ }
+
+resource "scalr_environment" "test" {
+  name       = "test-env4352345"
+  account_id = <acc-ID>
+  cost_estimation_enabled = true
+  
+}
+
+   
+resource "scalr_variable" "example" {
+  key          = "my_key_name"
+  value        = "my_value_name"
+  category     = "shell"
+  description  = "variable description"
+  workspace_id = <WS-ID>
+}
+resource "null_resource" "env_varstimeout0123456789timeout0123456789timeot0123456789timeout01234567" {
+  count = 1
+  triggers = {
+    run_id = "12"
+  }
+  provisioner "local-exec" {
+    command = "env"
   }
 }
-  
-provider "grafanaauth" {
-  url = "https://scalrlabs.grafana.net"
-//  organization_id = 2
-}
-
-resource "grafanaauth_api_key" "foo" {
-  name = "key_foo"
-  role = "Viewer"
-}
-
-
-output "api_key_foo_key_only" {
-  value = grafanaauth_api_key.foo.key
-  sensitive = true
-}
-
